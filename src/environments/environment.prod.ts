5b8601314bcfd0f40da5c77c0901d72590055358
mitverma <amit.wohlig@gmail.com>
2021-01-10 11:54:20 +0530

    dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlAddress: 'https://apisaiastroworld.com',
-  paymentUrlAddress: 'http://ec2-3-21-75-140.us-east-2.compute.amazonaws.com:3001'
+  paymentUrlAddress: 'https://apisaiastroworld.com/payment'
 };
